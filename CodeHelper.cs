80be149ca9550c8e4179cd90cf3e1e70b2a22b38
roboter <roboter@begemotik.ee>
2016-08-02 23:12:17 +0300

    Initial commit

---
+++ CodeHelper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+
+namespace DynCode
+{
+    public class CodeHelper
+    {
+        public static object HelperFunction(string classCode, string mainClass, object[] requiredAssemblies)
+        {
+            CSharpCodeProvider provider = new CSharpCodeProvider(new Dictionary<string, string> { { "CompilerVersion", "v4.0" } });
+
+            CompilerParameters parameters = new CompilerParameters
+            {
+                GenerateExecutable = false,       // Create a dll
+                GenerateInMemory = true,          // Create it in memory
+                WarningLevel = 3,                 // Default warning level
+                CompilerOptions = "/optimize",    // Optimize code
+                TreatWarningsAsErrors = false     // Better be false to avoid break in warnings
+            };
+
+            //----------------
+            // Add basic referenced assemblies
+            parameters.ReferencedAssemblies.Add("system.dll");
+            parameters.ReferencedAssemblies.Add("System.Windows.Forms.dll");
+            parameters.ReferencedAssemblies.Add("System.Core.dll");
+
+            //----------------
+            // Add all extra assemblies required
+            foreach (var extraAsm in requiredAssemblies)
+            {
+                parameters.ReferencedAssemblies.Add(extraAsm as string);
+            }
+
+            //--------------------
+            // Try to compile the code received
+            CompilerResults results = provider.CompileAssemblyFromSource(parameters, classCode);
+
+            //--------------------
+            // If the compilation returned error, then return the CompilerErrorCollection class with the errors to the caller
+            if (results.Errors.Count != 0)
+            {
+                return results.Errors;
+            }
+
+            //--------------------
+            // Return the created class instance to caller
+            return results.CompiledAssembly.CreateInstance(mainClass);
+        }
+    }
+}
